82d959503db1c5d8634105b99dc08c0a00e93d04
KIMO892004 <kimoragab2004@gmail.com>
2023-03-16 16:10:05 -0700

    tasks 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 /**
-*main-entry poit
-*return: alawys 0 (success)
-*/
+ * main-Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: alawys 0 (success)
+**/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
