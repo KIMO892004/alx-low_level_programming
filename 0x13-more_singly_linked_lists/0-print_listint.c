cfea9f913873d7d2f8285de30a4b23033284dcd2
KIMO892004 <kimoragab2004@gmail.com>
2023-05-01 16:15:17 -0700

    tasks

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a linked list
+ * @k: head of  the   list
+ *
+ * Return: the number of the nodes
+ */
+size_t print_listint(const listint_t *k)
+{
+const listint_t *cursor = k;
+size_t count = 0;
+
+while (cursor != NULL)
+{
+printf("%d\n", cursor->n);
+count += 1;
+cursor = cursor->next;
+}
+return (count);
+}
+
