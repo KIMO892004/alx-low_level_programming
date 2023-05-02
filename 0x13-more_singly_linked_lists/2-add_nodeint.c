4d385fed1a04da913ade925d84494e29a0d15b9a
KIMO892004 <kimoragab2004@gmail.com>
2023-05-02 14:34:53 -0700

    tasks

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -16,7 +16,7 @@ listint_t *add_nodeint(listint_t **head, const int r)
 
 	new_node->next = NULL;
 	new_node->r = r;
-	if (*headd)
+	if (*head)
 		new_node->next = *head;
 	*head = new_node;
 	return (new_node);
