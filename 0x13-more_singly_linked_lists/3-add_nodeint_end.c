7aa5e28467cb8bf6004064f0859ab667ee1bd7f9
KIMO892004 <kimoragab2004@gmail.com>
2023-05-02 14:52:16 -0700

    tasks

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -2,26 +2,26 @@
 
 /**
  * add_nodeint_end - adds a node to the bottom
- * @heading: the pointer to the first node
+ * @head: the pointer to the first node
  * @num: the value for new node
  *
  * Return: the pointer to new node
  */
-listint_t *add_nodeint_end(listint_t **heading, const int num)
+listint_t *add_nodeint_end(listint_t **head, const int num)
 {
 	listint_t *new_node = malloc(sizeof(listint_t));
 	listint_t *node;
 
-	if (!heading || !new_node)
+	if (!head || !new_node)
 		return (NULL);
 
 	new_node->next = NULL;
 	new_node->num = num;
-	if (!*heading)
-		*heading = new_node;
+	if (!*head)
+		*head = new_node;
 	else
 	{
-		node = *heading;
+		node = *head;
 		while (node->next)
 			node = node->next;
 		node->next = new_node;
