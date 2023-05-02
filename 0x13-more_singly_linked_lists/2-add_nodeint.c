371dac5df923620cc4d0329269a889d7030bee87
KIMO892004 <kimoragab2004@gmail.com>
2023-05-02 15:00:36 -0700

    tasks

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,19 +3,19 @@
 /**
  * add_nodeint - adds a node to the top
  * @head: the pointer to the first node
- * @num: the value for new node
+ * @n: the value for new node
  *
  * Return: the pointer to new node
  */
-listint_t *add_nodeint(listint_t **head, const int num)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node = malloc(sizeof(listint_t));
 
 	if (!head || !new_node)
-		return (NULL);
+		return(0);
 
-	new_node->next = NULL;
-	new_node->num = num;
+	new_node->next = 0;
+	new_node->n = n;
 	if (*head)
 		new_node->next = *head;
 	*head = new_node;
