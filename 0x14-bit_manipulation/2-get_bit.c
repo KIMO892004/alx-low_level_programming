509f04c5ae1efe46a869f3742591dd96a358e9b9
KIMO892004 <kimoragab2004@gmail.com>
2023-05-04 13:02:59 -0700

    tasks

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * get_bit - A function that gets a bit at index
+ * @n: the number to index
+ * @index: The index where the bit get at
+ * Return: The value of the bit or -1 if an error occured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int maximum = 0x01;
+
+	maximum <<= index;
+	if (maximum == 0)
+		return (-1);
+
+	if ((n & maximum))
+		return (1);
+	else
+		return (0);
+}
