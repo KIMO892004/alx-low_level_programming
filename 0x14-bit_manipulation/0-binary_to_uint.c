9a2ee22e240144cd86d098cf133941cea00da48d
KIMO892004 <kimoragab2004@gmail.com>
2023-05-04 12:49:56 -0700

    tasks

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,11 +11,11 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int k = 0;
 
 	if (!b)
-		return (NULL);
+		return (0);
 	while (*b)
 	{
 		if (*b != '0' && *b != '1')
-			return (NULL);
+			return (0);
 		k = k * 2 + (*b++ - '0');
 	}
 	return (k);
